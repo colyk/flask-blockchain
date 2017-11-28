6623d338cac79aa8a7ef2edf82268743c5a080f2
colyk <colyk@yandex.ru>
2017-11-28 17:11:58 +0100

    add proof

--- blockChain.py
+++ blockChain.py
@@ -9,7 +9,7 @@ def check_blocks_integrity():
     result = []
     for i in range(2, int(get_next_block())):
         tmp ={'block' : '', 'result' : ''}
-        cur_hash = json.load(open(BLOCKCHAIN_DIR + str(i) + '.json'))['hash']
+        cur_hash = json.load(open(BLOCKCHAIN_DIR + str(i) + '.json'))['prev_hash']
         prev_hash = hashlib.sha256(open(BLOCKCHAIN_DIR + str(i-1) + '.json', 'rb').read()).hexdigest()
         if cur_hash != prev_hash:
             tmp['block'] = str(i-1)
@@ -18,16 +18,14 @@ def check_blocks_integrity():
             tmp['block'] = str(i-1)
             tmp['result'] = 'ok'
         result.append(tmp)
-
     return result
 
 
 def check_block(index):
-    index = str(int(index) + 1)
-    result = []
+    index = str(int(index))
     index_ = str(int(index) - 1)
-    tmp ={'block' : '', 'result' : ''}
-    cur_hash = json.load(open(BLOCKCHAIN_DIR + index + '.json'))['hash']
+    tmp = {'block' : '', 'result' : ''}
+    cur_hash = json.load(open(BLOCKCHAIN_DIR + index + '.json'))['prev_hash']
     prev_hash = hashlib.sha256(open(BLOCKCHAIN_DIR + index_ + '.json', 'rb').read()).hexdigest()
     if cur_hash != prev_hash:
         tmp['block'] = index_
@@ -35,11 +33,11 @@ def check_block(index):
     else:
         tmp['block'] = index_
         tmp['result'] = 'ok'
-    result.append(tmp)
-    return result
+    return tmp
+
 
 def get_hash(file_name):
-    with open(BLOCKCHAIN_DIR + file_name + '.json', 'rb') as file:
+    with open(BLOCKCHAIN_DIR + str(file_name) + '.json', 'rb') as file:
         return hashlib.sha256(file.read()).hexdigest()
 
 
@@ -51,33 +49,52 @@ def get_next_block():
     return str(next_index)
 
 
-def get_work_proof(file_name, difficulty=1):
-    with open(BLOCKCHAIN_DIR + file_name + '.json', 'rb') as file:
-        if str(hashlib.sha256(file.read()).hexdigest())[:difficulty] == '0'*difficulty:
-            return true
+def is_valid_proof(last_proof, proof, difficulty):
+    guess = f'{last_proof}{proof}'.encode() 
+    guess_hash = hashlib.sha256(guess).hexdigest() 
+
+    return guess_hash[:difficulty] == '0' * difficulty
 
 
+def get_POW(file_name, difficulty=1):
+    # POW - proof of work
+    file_name = str(file_name)
+    if file_name.endswith('.json'):
+        file_name = int(file_name.split('.')[0])
+    else:
+        file_name = int(file_name)
 
-def write_block(text):
+    last_proof = json.load(open(BLOCKCHAIN_DIR + str(file_name - 1) + '.json'))['proof']
+    proof = 0
+    while is_valid_proof(last_proof, proof, difficulty) is False:
+        proof += 1
+    cur_block = json.load(open(BLOCKCHAIN_DIR + str(file_name) + '.json'))
+    cur_block['proof'] = proof
+    cur_block['prev_hash'] = get_hash(str(file_name - 1))
+    with open(BLOCKCHAIN_DIR + str(file_name) + '.json', 'w') as file:
+        json.dump(cur_block, file, indent=4, ensure_ascii=False)
+
+
+def write_block(text, make_proof=False):
     cur_index = get_next_block()
     prev_index = str(int(cur_index) - 1)
     prev_block_hash = get_hash(prev_index)
-    cur_block_hash = get_hash(cur_index)
+    # cur_block_hash = get_hash(cur_index)
     data = {'text' : text,
             'prev_hash' : prev_block_hash,
-            'cur_hash' : cur_block_hash,
             'timestamp' : time(),
-            'proof' : ' ',
+            'proof' : 0,
             'index' : cur_index
             }  
 
     with open(BLOCKCHAIN_DIR + cur_index + '.json', 'w') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
+    if make_proof is True:
+        get_POW(str(cur_index))
 
 
 if __name__ == '__main__':
     for i in range(2,10):
-        write_block(str(i))
-    print(check_blocks_integrity())
-
-
+        get_POW(str(i))
+    for i in range(2,10):
+        print(check_block(str(i)))
